aab4ae1b9999a5167bd0d06277f945ac2e29579c
Basil Mahmood <basilmahmood98@gmail.com>
2022-02-08 16:10:14 -0500

    added local db connection

--- server/src/config/mongo.ts
+++ server/src/config/mongo.ts
@@ -2,8 +2,17 @@ import Mongoose from 'mongoose';
 
 const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = process.env;
 
-const uri = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
-console.log(uri);
+let uri;
+// Connect locally to hardcoded string
+if (!DB_HOST) {
+  uri = `mongodb://localhost:27017/unity-assessment-basil?retryWrites=true&w=majority`;
+} 
+
+// Connect to remote
+else {
+  uri = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
+}
+
 Mongoose.connect(uri);
 
 const db = Mongoose.connection;
